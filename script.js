9368a76157a835f4a23affdf0719f2da742e6454
Dung Ngo <dungngotanlhp@gmail.com>
2020-07-07 17:36:38 +0700

    Updated Files

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
-const exchangeRate2Vnd = {'usd':23232, 'eur':26240, 'jpy':215.859, 'vnd':1};
+const exchangeRate2Vnd = { 'usd': 23232, 'eur': 26240, 'jpy': 215.859, 'vnd': 1 };
+let exchangeRates2Vnd = {};
+
+/*
+This function get the currency rates through API
+*/
+async function callAPI() {
+  let url = "http://data.fixer.io/api/latest?access_key=c00e15cca5a9a358410b21fe863681b1"
+  let response = await fetch(url);
+  if (response.ok) {
+    let data = await response.json();
+    exchangeRates2Vnd = data['rates'];
+  }
+}
+callAPI();
 
 /*
 This function is used to format the output
@@ -14,21 +28,22 @@ function formatCurrency(type, value) {
 /*
 This function is used to convert from 1 currency to another currency
 */
-function converter(){
-  let from = document.getElementById('from').value;
-  let to = document.getElementById('to').value;
+function converter() {
+  let from = document.getElementById('from').value.toUpperCase();
+  let to = document.getElementById('to').value.toUpperCase();
   let amount = parseInt(document.getElementById('dn-input-text').value);
 
-  if (amount < 0){
+  if (amount < 0) {
     alert("The input value cannot be negative");
+    document.getElementById('dn-input-text').value = '';
     return null;
-  }else if(isNaN(amount)){
+  } else if (isNaN(amount)) {
     alert("The input value is not a number");
+    document.getElementById('dn-input-text').value = '';
     return null;
   }
-  
-  convert_amount = amount * exchangeRate2Vnd[from] / exchangeRate2Vnd[to];
-  output = formatCurrency(from.toUpperCase(), amount) + " = " + formatCurrency(to.toUpperCase(), convert_amount.toFixed(2));
+  convert_amount = amount / exchangeRates2Vnd[from] * exchangeRates2Vnd[to];
+  output = formatCurrency(from, amount) + " = " + formatCurrency(to, convert_amount.toFixed(2));
   document.getElementById("dn-output-text").innerHTML = output; // innerHTML has to be String value
 }
 
@@ -38,19 +53,20 @@ This function is used to reverse the values appeared in the dropdown list
 function reverse() {
   let from = document.getElementById("from").value;
   let to = document.getElementById("to").value;
-  document.getElementById("from").value = to
-  document.getElementById("to").value = from
+  document.getElementById("from").value = to;
+  document.getElementById("to").value = from;
 }
 
-/*
-1. Use Quokka to check the result of a function or variable
-2. Equivalent: <button onclick='myFunction'></button> is equal to <script>document.getElementById("id here").addEventListener('type of event', myFunction())</script>
-3. To convert from String to Number, use parseInt() (if NaN, return NaN) or Number() (if NaN, return 0) method as above
-4. isNaN() is used to check if a number is NaN
-5. To change a current value appearing in dropdown list: we use document.getElementById("id here").value = new value
-6. We can see the ouput of console.log() in browser (Inspect element -> Console. Run the js file->Click on button to call function->Output showed in Console)
-7. Use querySelector with its attributes: 
-let var = document.querySelector('tagElement[attr1="value1"]:checked').value;
-8. Show a value of a variable in a String: (use backticks `` and ${}). Ex:
-document.getElementById('id here').innerHTML = `A String here ${variable}`
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content
 */
+function UpdateCurrency() {
+  cur_abbr = ['USD','EUR','JPY','AED','INR'];
+  cur = ['US Dollar', 'Euro', 'Japanese Yen', 'Emirati Dirham', 'Indian Rupee'];
+  ids = ['line1', 'line2', 'line3', 'line4', 'line5'];
+  for (i=0; i<cur_abbr.length; i++){
+    let toVND = 1 / exchangeRates2Vnd[cur_abbr[i]] * exchangeRates2Vnd['VND'];
+    toVND = toVND.toFixed(2);
+    document.getElementById(ids[i]).innerHTML = `${cur[i]}: 1 ${cur_abbr[i]} = ${toVND} VND`;
+  }
+}
